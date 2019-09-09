5b3386cbd6a11b07e230c8a3018272e0611a8cbe
naveensehgal <naveen.sehg@gmail.com>
2019-09-09 15:16:48 -0400

    Increase to 4 scale search to match paper.

--- python/body.py
+++ python/body.py
@@ -20,8 +20,7 @@ class Body(object):
         self.model.eval()
 
     def __call__(self, oriImg):
-        # scale_search = [0.5, 1.0, 1.5, 2.0]
-        scale_search = [0.5]
+        scale_search = [0.5, 1.0, 1.5, 2.0]
         boxsize = 368
         stride = 8
         padValue = 128
